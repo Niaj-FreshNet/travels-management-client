a28b3f9f0be10a10dc4ad5687fe7f80b84d1d907
Niaj-FreshNet <pensiveniaj@gmail.com>
2025-10-20 12:33:29 +0600

    lastly

--- src/Hooks/useAxiosUser.jsx
+++ src/Hooks/useAxiosUser.jsx
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const axiosUser = axios.create({
     // baseURL: 'http://51.79.201.174:30015',
-    // baseURL: 'https://api.quickway2services.com',
+    baseURL: 'https://api.quickway2services.com',
     // baseURL: 'https://travels-management-server.onrender.com',
-    baseURL: 'http://localhost:5000',
+    // baseURL: 'http://localhost:5000',
     // baseURL: 'https://travels-management-server.vercel.app',
     headers: {
         'Content-Type': 'application/json'
